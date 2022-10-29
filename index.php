d8f165cdbee0f5520e2407c8b2d373accd4b6394
gilangwokeh <gilangwokeh12@gmail.com>
2022-10-29 21:50:54 +0700

    create php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 $amin = "amin" ;
+//integer
+$hello = 22;
+//float
+$nilai_ipk = 3.99;
+//boolean
+$benar = true;
+$salah = false;
+
+var_dump($benar);
+echo "<br/>";
+var_dump($salah);
+echo "<br/>";
+echo "nilai ipk adalah $nilai_ipk <br/>";
+echo "total anak kamu adalah $hello <br/>";
 echo "$amin";
 echo "<h1>hello word</h1>";
 echo ("<h2>hello brother<h2>");
