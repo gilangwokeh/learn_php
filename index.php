c614e74dacd67fefe75783d15637796bd31ec6d8
gilangwokeh <gilangwokeh12@gmail.com>
2022-10-29 21:59:41 +0700

    create php

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ $nilai_ipk = 3.99;
 //boolean
 $benar = true;
 $salah = false;
-
+//array
+$mahasiswa = array(99.4,"joko",55);
+echo "$mahasiswa[2]<br/>";
+echo "$mahasiswa[0]<br/>";
 var_dump($benar);
 echo "<br/>";
 var_dump($salah);
