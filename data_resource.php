c0bfc3816003e9395ce6c10e687af5a738e1c636
gilangwokeh <gilangwokeh12@gmail.com>
2022-10-31 20:26:07 +0700

    update code

--- data_resource.php
+++ data_resource.php
@@ -1,5 +1,15 @@
 <?php
 //data resource 
-$koneksi = mysql_connect("localhost","root","");
-var_dump($koneksi);
+$servername = "127.0.0.1";
+$username = "gilang1";
+$password = "gilang12345";
+
+//create connection mysql
+$conn = new mysqli($servername,$username,$password);
+
+//check connection
+if($conn->connect_error){
+  die("Connection failed:".$conn->connect_error);
+}
+echo "connection successfully";
 ?>
