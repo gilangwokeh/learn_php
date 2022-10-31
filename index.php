869c88642c36db1ff3bd09c202088a25595ae03d
gilangwokeh <gilangwokeh12@gmail.com>
2022-10-31 09:08:12 +0700

    update code

--- index.php
+++ index.php
@@ -9,8 +9,37 @@ $benar = true;
 $salah = false;
 //array
 $mahasiswa = array(99.4,"joko",55);
+//add array
+$masasiswa[3] = "mobil";
+$masasiswa[4] = 10;
 echo "$mahasiswa[2]<br/>";
 echo "$mahasiswa[0]<br/>";
+//call add array
+print_r($masasiswa);
+echo "<br/>";
+
+//object 
+class mahasiswa{
+  public $nama;
+  public $umur;
+  public $tanggal_lahir;
+
+  function get_name(){
+    return $this-nama;
+  }
+}
+//return object
+$mahasiswa_abadi = new mahasiswa;
+$mahasiswa_abadi->nama = 'budeman';
+$mahasiswa_abadi->umur = 17;
+$mahasiswa_abadi->tanggal_lahir = "26 desember 1999";
+print_r($mahasiswa_abadi);
+echo "<br/>";
+
+//data resource 
+$koneksi = mysqli_connect("127.0.0.1","gilang1","gilang12345","karyawan");
+var_dump($koneksi);
+echo "<br/>";
 var_dump($benar);
 echo "<br/>";
 var_dump($salah);
